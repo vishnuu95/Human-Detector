820b7e9918b855d6254d5985ae83fe6cd508b323
Vasista <vasista1997@gmail.com>
2020-10-19 21:54:48 -0400

    declared variables and functions
    ioHandler.hpp declared individual attributes and functions

--- include/ioHandler.hpp
+++ include/ioHandler.hpp
@@ -9,8 +9,27 @@ namespace vision {
 
 class IOHandler {
  private:
+ 	string outPath = NULL
+ 	string inPath = NULL;
+ 	bool isImg = false, ifVisualize = false;
+ 	cv::VideoWriter* video;
 
  public:
+ 	IOHandler();
+ 	~IOHandler();
+ 	void argParse(int, **char);
+ 	bool getInputType();
+ 	bool ifVisualize();;
+ 	string getInFilePath();
+ 	string getOutFilePath();
+
+ 	void drawBb(vector<vector<int>>, cv::Mat);
+ 	void addFrame(cv::Mat);
+ 	static void seeImg(cv::Mat);
+ 	static void seeVideo(vector<cv::Mat>);
+ 	void saveImg(string, cv::Mat);
+ 	void saveVideo(string);
+
 };
 
 }
