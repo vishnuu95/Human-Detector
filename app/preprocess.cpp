d9802bdf0fadfedf7ff788f675091efba72028ef
Vasista <vasista1997@gmail.com>
2020-10-20 13:36:03 -0400

    defined stub function for Preprocess class
    app/preprocess.cpp - defined stub implementations for all functions in
    Preprocess class

---
+++ app/preprocess.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include "opencv2/opencv.hpp"
+#include "preprocess.hpp"
+
+using namespace vision;
+
+void Preprocessor::preprocess(pair<int, int> size, int blurKernel, cv::Mat& inImg, cv::Mat& outImg) {
+	return;	
+}
