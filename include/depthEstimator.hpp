c97c3177a0b7f064f39261cf8341f9e75c797db4
Vasista <vasista1997@gmail.com>
2020-10-19 22:01:27 -0400

    declared class functions and attribute
    inlcude/depthEstimator.hpp - declared class functions and attributes

--- include/depthEstimator.hpp
+++ include/depthEstimator.hpp
@@ -8,8 +8,14 @@ namespace vision {
 
 class DepthEstimator {
  private:
+ 	double humanHeight;
+ 	vector<double> intrinsicParams;
 
  public:
+ 	DepthEstimator();
+ 	~DepthEstimator();
+ 	vector<double> estimateDepth(vector<vector<int>>);
+ 	vector<vector<double>>  transform2dTo3d(vecotr<vector<int>>);
 };
 
 }
