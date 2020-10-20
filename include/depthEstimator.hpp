c1809dccc7c0501987b3c4d95442419d5fe16cd0
clueless-bachu <vasista1997@gmail.com>
2020-10-20 14:36:32 -0400

    Added Comments depthEstimator
    include/depthEstimator.hpp - Added comments

--- include/depthEstimator.hpp
+++ include/depthEstimator.hpp
@@ -1,19 +1,29 @@
+/**
+ *  @file    depthEstimator.hpp
+ *  @author  Vasista (clueless-bachu)
+ *  @author  Vishnuu (vishnuu95)
+ *  @date    10/20/2020
+ *  @brief Depth Estimation module definition under vision namespace. 
+ *	@copyright MIT License (c) 2020 Vasista and Vishnuu
+ */
 #pragma once
 
 using namespace std;
 
-namespace vision {
+namespace vision
+{
 
-class DepthEstimator {
- private:
- 	double humanHeight;
- 	vector<double> intrinsicParams;
+	class DepthEstimator
+	{
+	private:
+		double humanHeight;
+		vector<double> intrinsicParams;
 
- public:
- 	DepthEstimator();
- 	~DepthEstimator();
- 	vector<double> estimateDepth(vector<vector<int>>);
- 	vector<vector<double>>  transform2dTo3d(vector<vector<int>>);
-};
+	public:
+		DepthEstimator();
+		~DepthEstimator();
+		vector<double> estimateDepth(vector<vector<int>>);
+		vector<vector<double>> transform2dTo3d(vector<vector<int>>);
+	};
 
-}
+} // namespace vision
