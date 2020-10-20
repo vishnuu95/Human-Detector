da1bac6107198babe6b9e0e94a71e31cec6d4580
clueless-bachu <vasista1997@gmail.com>
2020-10-20 14:39:05 -0400

    Included transformation comments
    include/transformation.hpp - Added comments

--- include/transformation.hpp
+++ include/transformation.hpp
@@ -1,20 +1,30 @@
+/**
+ *  @file    transformation.hpp
+ *  @author  Vasista (clueless-bachu)
+ *  @author  Vishnuu (vishnuu95)
+ *  @brief Transformation module that transforms Human coordinates from Camera frame to Robot Frame. 
+ *	@copyright MIT License (c) 2020 Vasista and Vishnuu.
+ */
 #pragma once
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-namespace vision {
+namespace vision
+{
 
-class Transformation {
- private:
- 	Eigen::Matrix4f* transform;
- public:
- 	Transformation();
- 	~Transformation();
- 	void setTransform(Eigen::Matrix4f);
- 	Eigen::Matrix4f getTransform();
- 	Eigen::Vector4f Transform(Eigen::Vector4f);
-};
+	class Transformation
+	{
+	private:
+		Eigen::Matrix4f *transform;
 
-}
+	public:
+		Transformation();
+		~Transformation();
+		void setTransform(Eigen::Matrix4f);
+		Eigen::Matrix4f getTransform();
+		Eigen::Vector4f Transform(Eigen::Vector4f);
+	};
+
+} // namespace vision
