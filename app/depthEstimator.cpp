7b27ea9c855cbab955c65c988c52e2fd8a1becc8
Vasista <vasista1997@gmail.com>
2020-10-20 13:32:45 -0400

    defined and implemented stub functions fro depthEstimator.hpp
    app/depthEstimator.cpp - defined all functions as stub implementations
    for the declared functions in the .hpp file

---
+++ app/depthEstimator.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "depthEstimator.hpp"
+
+using namespace vision;
+using namespace std;
+
+DepthEstimator::DepthEstimator() {
+
+}
+
+DepthEstimator::~DepthEstimator() {
+	
+}
+
+vector<double> DepthEstimator::estimateDepth(vector<vector<int>>) {
+	vector<double> depths;
+	return depths;
+}
+
+vector<vector<double>> DepthEstimator::transform2dTo3d(vector<vector<int>>) {
+	vector<vector<double>> cords3d;
+	return cords3d;
+}
