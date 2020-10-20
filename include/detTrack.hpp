e0bfccd3e2457986ad691db17aed5e085b5a8b3e
Vasista <vasista1997@gmail.com>
2020-10-19 22:00:11 -0400

    added attributes and functions
    include/detTrack.hpp - declared class atrributes and functions

--- include/detTrack.hpp
+++ include/detTrack.hpp
@@ -8,7 +8,14 @@ namespace vision {
 
 class DetTrack {
  private:
+ 	cv::MultiTracker* trackers;
+ 	cv::dnn::Net* yoloModel;
  public:
+ 	DetTrack(string);
+ 	~DetTrack();
+ 	void addTrackers(vector<vector<int>>, vector<vector<int>>);
+ 	vector<vector<int>> detectHumans(cv::Mat);
+ 	vector<vector<int>> trackHumans(cv::Mat);
 };
 
 }
