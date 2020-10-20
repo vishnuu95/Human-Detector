1240701f792c93add7e5d1cffa3ebf46f0bd9eef
clueless-bachu <vasista1997@gmail.com>
2020-10-20 14:37:46 -0400

    Added comments preprocess.hpp
    include/preprocess.hpp - Added comments

--- include/preprocess.hpp
+++ include/preprocess.hpp
@@ -1,14 +1,22 @@
+/**
+ *  @file    preprocess.hpp
+ *  @author  Vasista (clueless-bachu)
+ *  @author  Vishnuu (vishnuu95)
+ *  @brief preprocess Module that preprocesses the input image helping the detection module
+ *	@copyright MIT License (c) 2020 Vasista and Vishnuu
+ */
 #pragma once
 
 using namespace std;
 
-namespace vision {
+namespace vision
+{
 
-class Preprocessor {
- private:
+	class Preprocessor
+	{
+	private:
+	public:
+		static void preprocess(pair<int, int>, int, cv::Mat &, cv::Mat &);
+	};
 
- public:
- 	static void preprocess(pair<int, int>, int, cv::Mat&, cv::Mat&);
-};
-
-}
+} // namespace vision
