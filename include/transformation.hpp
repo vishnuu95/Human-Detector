01d9a4399ab55c6fc1c1f7ae87da09afc86d8300
Vasista <vasista1997@gmail.com>
2020-10-19 22:02:50 -0400

    Declared class attributes and functions
    include/transformation.hpp - Declared class attributes and functions

--- include/transformation.hpp
+++ include/transformation.hpp
@@ -8,7 +8,13 @@ namespace vision {
 
 class Transformation {
  private:
+ 	Eigen::Matrix4f* transform;
  public:
+ 	Transformation();
+ 	~Transformation();
+ 	void setTransform(Eigen::Matrix4f);
+ 	Eigen::Matrix4f getTransform();
+ 	Eigen::Vector4f transform(Eigen::Vector4f);
 };
 
 }
